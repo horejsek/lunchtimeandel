bc7e2e2e285a21340315054b863f44619d2af80d
Michal Horejsek <horejsekmichal@gmail.com>
2013-07-11 20:01:23 +0200

    Draft of selection

--- locales/cs.js
+++ locales/cs.js
@@ -9,5 +9,6 @@
 	"Get random meal with information about restaurant.": "Získat náhodný oběd s informací o restauraci.",
 	"Loading…": "Načítání…",
 	"For properly function, please, allow JavaScript for this site.": "Pro správnou funkčnost, prosím, povolte JavaScript.",
-	"This page takes some money for hosting and time for developing. I would appreciate any tip. Thank you!": "Tato stránka si žádá peníze za hosting a čas na vývoj. Ocením jakékoliv dýško. Děkuju!"
+	"This page takes some money for hosting and time for developing. I would appreciate any tip. Thank you!": "Tato stránka si žádá peníze za hosting a čas na vývoj. Ocením jakékoliv dýško. Děkuju!",
+	"Show selection": "Zobrazit výběr"
 }
